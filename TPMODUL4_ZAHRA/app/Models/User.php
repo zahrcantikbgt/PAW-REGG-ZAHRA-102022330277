aae74d411aa3499022b3a951a58f95dbb9401d73
Zahra Annisa Inayah <zahrannisayh28@gmail.com>
2025-04-27 22:57:58 +0700

    TPMODUL4_ZAHRA

---
+++ TPMODUL4_ZAHRA/app/Models/User.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    // 1. Tambahkan kolom 'name', 'email' dan 'phone' pada tabel yang boleh diisi
+    protected $fillable = ['name', 'email', 'phone'
+        // isi disini
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+}
