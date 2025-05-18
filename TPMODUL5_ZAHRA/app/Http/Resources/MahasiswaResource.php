92a49ac7111e0a683f2c8b55a6d58d292c940c47
Zahra Annisa Inayah <zahrannisayh28@gmail.com>
2025-05-18 14:49:01 +0700

    TPMODUL5_ZAHRA

---
+++ TPMODUL5_ZAHRA/app/Http/Resources/MahasiswaResource.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class MahasiswaResource extends JsonResource
+{
+    // TODO: Deklarasikan 3 properties (status, message, resource) dengan visibility public 
+    public $status;
+    public $message;
+    public $resource;
+
+
+    public function __construct($status, $message, $resource)
+    {
+        parent::__construct($resource);
+        $this->status  = $status;
+        $this->message = $message;
+    }
+
+    public function toArray(Request $request): array
+    {
+        // TODO: Lengkapi return dengan success mengambil dari property status, message dari property message, dan data dari property resource
+        return [
+            'success'   => $this->status,
+            'message'   => $this->message,
+            'data'      => $this->resource,
+
+        ];
+    }
+}
